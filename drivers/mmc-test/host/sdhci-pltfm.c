f91a44052962573f09d8b68ebfbd529bc4b2f23c
174high <466243346@qq.com>
2020-12-02 13:07:55 +0800

    add more

---
+++ drivers/mmc-test/host/sdhci-pltfm.c
@@ -0,0 +1,13 @@
+
+struct sdhci_host *sdhci_pltfm_init(struct platform_device *pdev,
+                                    const struct sdhci_pltfm_data *pdata,
+                                    size_t priv_size)
+{
+
+
+}
+
+
+EXPORT_SYMBOL_GPL(sdhci_pltfm_init);
+
+
